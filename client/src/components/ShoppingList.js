55ef02fcf34ac379b536dc8051e9baa776873d43
Lukas Freimonas <lukas.freimonas@manounija.lt>
2020-01-11 22:25:07 +0200

    ADD item functionality

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,39 +1,27 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import uuid from "uuid";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus } from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
-import { getItems } from "../actions/itemActions";
+import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
+import ItemModal from "../components/ItemModal";
 
 class ShoppingList extends Component {
 	componentDidMount() {
-		// this.props.getItems();
-		console.log(this.props)
+		this.props.getItems();
 	}
+
+	onDeleteClick = id => {
+		this.props.deleteItem(id);
+	};
 
 	render() {
 		const { items } = this.props.item;
 		return (
 			<Container>
-				<Button
-					color="dark"
-					style={{ marginBottom: "2rem" }}
-					onClick={() => {
-						const name = prompt("Enter Item");
-						if (name) {
-							this.setState(state => ({
-								items: [...state.items, { id: uuid(), name }]
-							}));
-						}
-					}}
-				>
-					Add Item
-				</Button>
-
+				<ItemModal />
 				<ListGroup>
 					<TransitionGroup className="shopping-list">
 						{items.map(({ id, name }) => (
@@ -43,11 +31,7 @@ class ShoppingList extends Component {
 										className="remove-btn"
 										color="danger"
 										size="sm"
-										onClick={() => {
-											this.setState(state => ({
-												items: state.items.filter(item => item.id !== id)
-											}));
-										}}
+										onClick={() => this.onDeleteClick(id)}
 									>
 										<FontAwesomeIcon icon={faMinus} />
 									</Button>
@@ -69,4 +53,4 @@ ShoppingList.propTypes = {
 
 const mapStateToProps = state => ({ item: state.item });
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 		{ id: uuid(), name: "Eggs" },
 		{ id: uuid(), name: "Milk" },
 		{ id: uuid(), name: "Steak" },
-		{ id: uuid(), name: "Water" }
+		{ id: uuid(), name: "Watermelon" }
 	]
 };
 
@@ -14,6 +14,14 @@ export default function(state = initialState, action) {
 	switch (action.type) {
 		case GET_ITEMS:
 			return { ...state };
+		case DELETE_ITEM:
+			return {
+				...state,
+				items: state.items.filter(item => item.id !== action.payload)
+			};
+
+		case ADD_ITEM:
+			return { ...state, items: [...state.items, action.payload] };
 
 		default:
 			return { ...state };
