7818e80d33eda7743a9be24f4965705da19bead1
Lukas Freimonas <lukas.freimonas@manounija.lt>
2020-01-12 00:19:10 +0200

    Heroku changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const items = require("./routes/api/items");
+const path = require("path");
 
 const app = express();
 
@@ -9,11 +10,22 @@ app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("DB connected..."))
 	.catch(err => console.log(err));
 
 app.use("/api/items", items);
+
+// Serve static assets
+if (process.env.NODE_ENV === "production") {
+	//Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
